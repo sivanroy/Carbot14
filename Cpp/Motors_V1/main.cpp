6196761ffbd65e4ce56dfce0351a770d642cecaf
libertlo <60669474+libertlo@users.noreply.github.com>
2022-02-13 15:33:56 +0100

    Add Motors_V1, use of softPwm.h

---
+++ Cpp/Motors_V1/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <unistd.h>
+#include "DualMC33926RPi.h"
+
+/*
+ * Cmd to run :
+ * $ gcc -o exec main.cpp -lwiringPi
+ * $ sudo ./exec
+ */
+
+int main() {
+
+    int time = 1;
+    int speed = 1;
+
+    DualMC33926RPi wheels;
+
+    wheels.init();
+
+    std::cout << "init" << "\n";
+
+    wheels.setM1Speed(speed);
+    usleep(1000000 * time);
+
+    wheels.setM1Speed(-speed);
+    usleep(1000000 * time);
+
+    wheels.stop();
+
+    wheels.setM2Speed(speed);
+    usleep(1000000 * time);
+
+    wheels.setM2Speed(-speed);
+    usleep(1000000 * time);
+
+    wheels.stop();
+
+    wheels.setSpeeds(speed, speed);
+    usleep(1000000 * time);
+
+    wheels.setSpeeds(-speed, -speed);
+    usleep(1000000 * time);
+
+    wheels.stop();
+
+    return 0;
+}
