f3df56c1c4170da75c2a1bb6dd1547085167594d
sivanroy <vanroy.simon@outlook.com>
2022-02-17 10:38:32 +0100

    update py

--- py/DemoFirst.py
+++ py/DemoFirst.py
@@ -1,9 +1,10 @@
 from controlledWheels import *
-from DE02Rpi import *
+#from DE02Rpi import *
+from FPGA_Rpi import *
 
 cw = ControlledWheels()
 cw.start()
-dr = DE02Rpi()
+#dr = DE02Rpi()
 speeds = [[0,0.2,0.2],[0.2,0,0.2]]
 dt = 1e-1
 
@@ -14,8 +15,11 @@ for i in range (len(speeds[0])):
 	cw.setSpeed(sl,sr)
 	for j in range(100): #stay 1s on each value
 		for i in range(int(dt/deltat)):
-			l = dr.measure(1,1)
-			r = dr.measure(1,0)
+			#l = dr.measure(1,1)
+			#r = dr.measure(1,0)
+			A1,B1,A2,B2 = countPi()
+			l = (A1 + B1) * 2
+			r = (A2 + B2) * 2
 			sl = l*wheelDia/2*RadPerTickEnc/deltat #check ??
 			sr = -r*wheelDia/2*RadPerTickEnc/deltat
 			if(abs(sl)>5):
