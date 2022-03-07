2f049ef54cbbc9d2c568228fbacfb9c395136a0e
libertlo <louis.libert@student.uclouvain.be>
2022-03-07 21:14:18 +0100

    lidar py and cpp for S6

---
+++ S6/lidar_cpp/lidar_test.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Louis Libert on 5/03/22.
+//
+
+#include <iostream>
+#include <errno.h>
+#include <wiringPiSPI.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "RPLidar/rplidar.h"
+
+int main() {
+
+    printf("Compilation terminated\n");
+    return 0;
+}
+
