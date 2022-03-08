c5c1cdf8b9725c8e1e212a2299e0cdce3fe0b5ff
thclayson <100210832+thclayson@users.noreply.github.com>
2022-03-08 14:31:19 +0100

    up

--- py/Path_Planning_Pot_field/Pot_fiel_V2.py
+++ py/Path_Planning_Pot_field/Pot_fiel_V2.py
@@ -11,94 +11,75 @@ import matplotlib.pyplot as plt
 from sympy import *
 from sympy.geometry import *
 
-ETHA = 0.2       # Scaling factor repulsive potential
-ALPHA = 2        # Scaling factor attractive potential
-d_limit = 2      # Composite potential field limit
-front_obst = 5   # Influence dimension of obstacle
-max_pot_value = 5 # Maximum value of the potential representing objects
-
-R = 0.02         # obstacle radius TO BE MODIFY
-
-grid_x_size = 3 # size [m]
-grid_y_size = 2 # size [m]
-
-res = 0.01      # meshing resolution [mm]
-
-def calc_AttractivePotential(x,y,x_goal,y_goal,d_limit):
+def calc_AttractivePotential(x,y,x_goal,y_goal,R):
+    
+    ALPHA = 2        # Scaling factor attractive potential
+    d_limit = 2      # Composite potential field limit
     
     # Distance to goal
     d = np.hypot(x - x_goal , y - y_goal)
     
     if d <= d_limit:
         
-        return  0.5*ALPHA*d*d
+        Fx = ALPHA*(x - x_goal)
+        Fy = ALPHA*(y - y_goal)
+        
+        return  Fx,Fy
     
     else:
-        return (d_limit*ALPHA*d)-(0.5*ALPHA*d_limit*d_limit) 
+        
+        Fx = (d_limit * ALPHA * (x - x_goal))/(d)
+        Fy = (d_limit * ALPHA * (y - y_goal))/(d)
+        
+        return  Fx,Fy
 
-def calc_RepulsivePotential(x,y,x_obst,y_obst,front_obst):
+def calc_RepulsivePotential(x,y,x_obst,y_obst,R):
     
-    # Search the nearest obst
-    minid = -1
-    dmin = float("inf")
-    for i, _ in enumerate(x_obst):
-        d = np.hypot(x - x_obst[i], y - y_obst[i])
-        if dmin >= d:
-            dmin = d
-            minid = i
+    ETHA = 0.2       # Scaling factor repulsive potential
+    front_obst = 5   # Influence dimension of obstacle
     
-    # Distance to obstacle
-    d = np.hypot(x - x_obst[minid] , y - y_obst[minid])
+    radius_obst = 1       # Obstacle radius
     
-    if R < d <= front_obst:
+    Fx = 0.0
+    Fy = 0.0
+    
+    for i in range(x_obst): 
         
-        pot =  0.5*ETHA*(1/d-1/front_obst)**2
+        # Distance to obstacle i
+        d = np.hypot(x - x_obst[i] , y - y_obst[i])
         
-        if pot < max_pot_value:
+        # If in robot range
+        if d <= R:
             
-            return pot
+            if radius_obst < d <= front_obst:
+                
+                Fx += ETHA * (1/front_obst - 1/d) * 1/(d*d) * (x-x_obst)/2
+                Fy += ETHA * (1/front_obst - 1/d) * 1/(d*d) * (y-y_obst)/2
+            
+        # If not in robot range
         else:
             
-            return max_pot_value
+            continue
+        
+    return Fx,Fy
+        
+        
+def main(x,y,xg,yg,xo,yo,R):
     
-    elif d < R:
-        
-        return max_pot_value
-        
-    else:
-        return 0
-        
-def main():
+    F_att_x,F_att_y = calc_AttractivePotential(x,y,xg,yg,d_limit,R)
+    F_rep_x,F_rep_y = calc_RepulsivePotential(x,y,xo,yo,front_obst,R)
     
-    x = np.arange(0,grid_x_size,res)
-    y = np.arange(0,grid_y_size,res)
-
-    xg,yg = 2.5,0.25
-    xo,yo = [0,0.5],[0,0.5]
-
-    X,Y = np.meshgrid(x,y)
-    pmap = np.zeros((len(y),len(x)))
+    F_tot_x = F_att_x + F_rep_x
+    F_tot_y = F_att_y + F_rep_y
     
-    for i in range(len(x)):
-        for j in range(len(y)):
-            
-            U_att = calc_AttractivePotential(x[i],y[j],xg,yg,d_limit)
-            U_rep = calc_RepulsivePotential(x[i],y[j],xo,yo,front_obst)
-
-            U_tot = U_rep
-
-            pmap[j][i] = U_tot
-        
-              
-    fig = plt.figure(figsize =(14, 9))
-
-    ax = plt.axes(projection='3d')
-    ax = plt.axes(projection='3d')
-    ax = plt.axes(projection='3d')
-    ax.plot_surface(X, Y, pmap ,rstride=1, cstride=1,cmap='viridis', edgecolor='none')
-    ax.set_title('Potential Field');
+    gain_vx = 1
+    gain_vy = 1
+    
+    v_x = gain_vx * F_tot_x
+    v_y = gain_vy * F_tot_y
+       
     return
 
 main();
-    
-    
+
+ 
